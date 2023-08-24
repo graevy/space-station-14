bc0aaaee3857b951a67008e7eef6eea54774862b
avery <avry@pm.me>
2023-08-24 00:08:01 -0700

    remove duplicate events

--- Content.Server/Shuttles/Events/ShuttleTimerLocalEvent.cs
+++
@@ -1,9 +0,0 @@
-using Content.Server.Shuttles.Systems;
-
-namespace Content.Server.Shuttles.Events;
-
-/// <summary>
-/// Optionally raised when a Timer queues a <see cref="ShuttleSystem.FasterThanLight"/> action.
-/// </summary>
-[ByRefEvent]
-public readonly record struct LocalShuttleTimerEvent(TimeSpan Countdown);
